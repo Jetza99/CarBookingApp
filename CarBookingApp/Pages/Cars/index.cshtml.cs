0ff512cb66ae79f3c9e9d0a38310dd28c0677cde
Black <Black@OMAR>
2023-08-23 16:26:49 +0100

    Add project files.

---
+++ CarBookingApp/Pages/Cars/index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CarBookingAppData;
+
+namespace CarBookingApp.Pages.Cars
+{
+    public class indexModel : PageModel
+    {
+        private readonly CarBookingAppData.CarBookingAppDbContext _context;
+
+        public indexModel(CarBookingAppData.CarBookingAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Car> Cars { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Cars != null)
+            {
+                Cars = await _context.Cars.ToListAsync();
+            }
+        }
+    }
+}
---
+++ CarBookingAppData/CarBookingAppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookingAppData
+{
+    public class CarBookingAppDbContext : DbContext
+    {
+       
+        public CarBookingAppDbContext(DbContextOptions<CarBookingAppDbContext> options) 
+            : base(options)
+        {
+        }
+        
+            
+        public DbSet<Car> Cars { get; set; }
+
+
+    }
+    public class CarBookingAppDbContextFactory : IDesignTimeDbContextFactory<CarBookingAppDbContext>
+    {
+        public CarBookingAppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CarBookingAppDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EcommerceDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new CarBookingAppDbContext(optionsBuilder.Options);
+        }
+    }
+
+}
